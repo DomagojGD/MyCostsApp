7e6b648ef11622212b0fe8d789505be874450195
DomagojGD <domagojzovko96@gmail.com>
2023-03-01 21:48:59 +0100

    Date picker and cost amount edit text added in AddNewCostActivity.kt

--- app/src/main/java/com/example/mycostsapp/activities/AddNewCostActivity.kt
+++ app/src/main/java/com/example/mycostsapp/activities/AddNewCostActivity.kt
@@ -1,12 +1,20 @@
 package com.example.mycostsapp.activities
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.example.mycostsapp.R
 import com.example.mycostsapp.databinding.ActivityAddNewCostBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 private lateinit var binding: ActivityAddNewCostBinding
+
+//Set calendar variable
+private var calendar = Calendar.getInstance()
+//Set date listener variable
+private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
 
 class AddNewCostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +28,14 @@ class AddNewCostActivity : AppCompatActivity() {
             supportActionBar?.title = "Add New Cost"
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
         }
+
+        //Set back button on toolbar
         binding.toolbarAddNewCost.setNavigationOnClickListener {
             onBackPressed()
         }
 
+        //When 'groceries' button is clicked, its color turns green and the rest white.
+        // Also, button text turns white, and text of rest of the buttons turns green.
         binding.btnGroceries.setOnClickListener {
             it.setBackgroundResource(R.drawable.light_green_background)
             binding.btnCar.setBackgroundResource(R.drawable.btn_white_background)
@@ -36,6 +48,8 @@ class AddNewCostActivity : AppCompatActivity() {
             binding.btnOther.setTextColor(ContextCompat.getColor(this, R.color.base_green))
         }
 
+        //When 'car' button is clicked, its color turns green and the rest white.
+        // Also, button text turns white, and text of rest of the buttons turns green.
         binding.btnCar.setOnClickListener {
             it.setBackgroundResource(R.drawable.light_green_background)
             binding.btnGroceries.setBackgroundResource(R.drawable.btn_white_background)
@@ -48,6 +62,8 @@ class AddNewCostActivity : AppCompatActivity() {
             binding.btnOther.setTextColor(ContextCompat.getColor(this, R.color.base_green))
         }
 
+        //When 'bills' button is clicked, its color turns green and the rest white.
+        // Also, button text turns white, and text of rest of the buttons turns green.
         binding.btnBills.setOnClickListener {
             it.setBackgroundResource(R.drawable.light_green_background)
             binding.btnCar.setBackgroundResource(R.drawable.btn_white_background)
@@ -60,6 +76,8 @@ class AddNewCostActivity : AppCompatActivity() {
             binding.btnOther.setTextColor(ContextCompat.getColor(this, R.color.base_green))
         }
 
+        //When 'other' button is clicked, its color turns green and the rest white.
+        // Also, button text turns white, and text of rest of the buttons turns green.
         binding.btnOther.setOnClickListener {
             it.setBackgroundResource(R.drawable.light_green_background)
             binding.btnCar.setBackgroundResource(R.drawable.btn_white_background)
@@ -72,6 +90,26 @@ class AddNewCostActivity : AppCompatActivity() {
             binding.btnOther.setTextColor(ContextCompat.getColor(this, R.color.white))
         }
 
-        //TODO Dodaj opise i date picker dialog
+        //Show date picker dialog when 'etEnterCostDate' is clicked.
+        binding.etEnterCostDate.setOnClickListener {
+
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
+
+            //Set date set listener: Create date picker dialog.
+            // When the date is chosen, text of 'etEnterCostDate' will be changed accordingly.
+            dateSetListener = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+
+                calendar.set(Calendar.YEAR, year)
+                calendar.set(Calendar.MONTH, month)
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+                val sdf = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
+                binding.etEnterCostDate.setText(sdf.format(calendar.time).toString())
+            }
+
+            DatePickerDialog(this@AddNewCostActivity, dateSetListener, year, month, dayOfMonth).show()
+        }
     }
 }
